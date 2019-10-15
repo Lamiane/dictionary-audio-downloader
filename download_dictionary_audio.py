c1f69b3e70360a40e317f90a42059cec925ac9e7
Agnieszka Pocha <lamiane.lammy@gmail.com>
2019-10-16 00:47:54 +0200

    downloads files, but needs fixes if word has multiple spellings, ex. muse (noun) and muse (verb)

--- download_dictionary_audio.py
+++ download_dictionary_audio.py
@@ -1,17 +1,22 @@
-# usage: download_dictionary_audio.py word_list words.txt
-# usage: download_dictionary_audio.py word_list words.txt --mode ogg
-# usage: download_dictionary_audio.py word_list words.txt --path /dictionary/to/store/stuff
+# usage: download_dictionary_audio.py examplary_word_list
+# usage: download_dictionary_audio.py examplary_word_list --mode ogg
+# usage: download_dictionary_audio.py examplary_word_list --path /directory/to/store/stuff
 
+import sys
 
 import os
+import re
 import argparse
 import traceback
-import urllib2
+import urllib.request
 import wget
 
 
 dictionary_url = "https://www.dictionary.com/browse/"
+prefix = "<audio preload"
+sufix = "</audio>"
 
+all_modes = ["mp3", "ogg"]
 
 def download_and_save(url, download_directory):
     ### returns 1 for success, 0 for failure
@@ -23,8 +28,25 @@ def download_and_save(url, download_directory):
         return 0
     
 
-def filename_wrapper(word, path):
-    return os.path.join([path, word+"."+mode])
+def filename_wrapper(word, path, extension):
+    return os.path.join(path, word+"."+extension)
+
+
+def extract_urls(text):
+    pre = "src="
+    suf = " t"
+    pattern = re.compile(pre+".*?"+suf)
+    all_urls = [x.strip(pre).strip(suf).strip("\"") for x in pattern.findall(text)]
+    urls = {}
+    for mode in all_modes:
+        urls[mode] = [x for x in all_urls if mode in x]
+        if len(urls[mode]) == 0:
+            urls.pop(mode)
+        elif len(urls[mode]) == 1:
+            urls[mode] = urls[mode][0]
+        else:
+            print(f"problems, problems, problems with urls,\n{urls[mode]}")
+    return urls
 
 
 if __name__=="__main__":
@@ -35,15 +57,13 @@ if __name__=="__main__":
     parser.add_argument("--path", type=str, default=".", help="dictionary where files shall be saved, must exist, default is the directory containing the script")
     
     args = parser.parse_args()
-
-    supplementary_modes = ["mp3", "ogg"]
+    
+    supplementary_modes = list(all_modes)
     supplementary_modes.remove(args.mode)
     
     # read words from file
     with open(args.word_list, "r") as f:
         all_words = f.readlines()
-
-    print("all_words", all_words)
 
     non_default_mode = []
     not_in_dictionary = []
@@ -55,47 +75,37 @@ if __name__=="__main__":
         # get the dictionary.com HTML
         try:
             # retreive HTML
-            responose = urllib2.urlopen(dictionary_url+word)
-            html_content = responose.read()
+            responose = urllib.request.urlopen(dictionary_url+word)
+            html_content = responose.read().decode('utf-8')
         except:
             traceback.print_exc()
             not_in_dictionary.append(word)
             continue
-        
-        # parse HTML
-        audio_lines = []
-        for line in html_content:
-            if args.mode in line:
-                print(line)  # TODO remove this debug
-                audio_lines.add(line)
-        
+       
+        # parse html
+        pattern = re.compile(prefix+".*?"+sufix)
+        audio_lines = pattern.findall(html_content)
+
         if len(audio_lines) == 1:
-            success = download_and_save(filename_wrapper(audio_lines[0], args.path))
+            urls = extract_urls(audio_lines[0])
+            if args.mode in urls:
+                mode = args.mode
+            else:
+                mode = list(urls.keys())[0]
+            success = download_and_save(urls[mode], filename_wrapper(word, args.path, mode))
             if success:
-                all_went_well.add(word)
-                continue
+                if mode == args.mode:
+                    all_went_well.append(word)
+                else:
+                    non_default_mode.append(word) 
             
         elif len(audio_lines) > 1:
-            print("problems, problems, too many audio_lines")
+            print(f"\nproblems, problems, too many audio_lines for word {word}")
             print(audio_lines)
-            continue
-        
         else:
             # no audio_lines have been found
-            for m in supplementary_modes:
-                for line in html_content:
-                    if m in line:
-                        audio_lines.add(line)
-        
-            if len(audio_lines) < 1:
-                no_audio.add(word)
-            else:
-                success = download_and_save(filename_wrapper(audio_lines, args.path))
-                if success:
-                    non_default_mode.add(word)
-                else:
-                    no_audio.add(word)
-        
+            no_audio.append(word)
+    
     assert (set(all_words) == set(not_in_dictionary+non_default_mode+no_audio+all_went_well)), print(f"Pocha, sth went wrong, \n{set(all_words)},\n{set(not_in_dictionary+non_default_mode+no_audio+all_went_well)}")
 
     print(f"the following words are not in the dictionary: {not_in_dictionary}")
